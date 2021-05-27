cb4c13ba37a8dc6f75658174b7d428f48d961ed3
jolazabal87 <ignacioolazabal@hotmail.com>
2021-05-27 20:57:46 -0300

    deploying

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.static(path.resolve(__dirname,"./public")))
 app.set("port", process.env.PORT || 3000)
-app.listen(app.get("port"), () => console.log("Server starts in http://localhost" + app.get("port")));
+app.listen(app.get("port"), () => console.log("Server starts in http://localhost:" + app.get("port")));
 app.get("/", (req,res) => res.sendFile(path.resolve(__dirname,"./views/home.html")));
 app.get("/login", (req,res) => res.sendFile(path.resolve(__dirname,"./views/login.html")));
 app.get("/register", (req,res) => res.sendFile(path.resolve(__dirname,"./views/register.html")));
