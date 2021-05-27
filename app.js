98db234e63c766e87e4452bf8e4994b7846e0639
jolazabal87 <ignacioolazabal@hotmail.com>
2021-05-27 10:47:24 -0300

    subiendo version 1

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const { dirname } = require('path');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.resolve(__dirname,"./public")))
+app.listen(3000, () => console.log("Server starts in http://localhost:3000"));
+app.get("/", (req,res) => res.sendFile(path.resolve(__dirname,"./views/home.html")));
+app.get("/login", (req,res) => res.sendFile(path.resolve(__dirname,"./views/login.html")));
+app.get("/register", (req,res) => res.sendFile(path.resolve(__dirname,"./views/register.html")));
