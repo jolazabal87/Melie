8171d78413d1f5b7d0668f1eadc1ca3e4c5a637c
jolazabal87 <ignacioolazabal@hotmail.com>
2021-05-27 20:12:04 -0300

    probando HEROKU

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const path = require('path');
 const app = express();
 
 app.use(express.static(path.resolve(__dirname,"./public")))
-app.listen(3000, () => console.log("Server starts in http://localhost:3000"));
+app.set("port", process.env.PORT || 3000)
+app.listen(app.get("port"), () => console.log("Server starts in http://localhost" + app.get("port")));
 app.get("/", (req,res) => res.sendFile(path.resolve(__dirname,"./views/home.html")));
 app.get("/login", (req,res) => res.sendFile(path.resolve(__dirname,"./views/login.html")));
 app.get("/register", (req,res) => res.sendFile(path.resolve(__dirname,"./views/register.html")));
